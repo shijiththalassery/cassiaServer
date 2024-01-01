b9fb6ffc59eb7e8a036bae5aac3ea6efb24f2e87
shijith pk <shijith.thalassery@gmail.com>
2024-01-01 22:08:45 +0530

    nodeMailer

--- index.js
+++ index.js
@@ -2,12 +2,78 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const twilio = require('twilio');
+const fs = require('fs');
+const path = require('path');
+const handlebars = require('handlebars');
+const nodemailer = require('nodemailer');
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-    console.log('okey');
-    res.json('okey');
+const accountSid = process.env.VITE_API_ACCOUNT_SID;
+const authToken = process.env.VITE_API_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+app.get("/about", async(req, res) => {
+    console.log('entering body');
+    res.json('okey shijith')
+
+});
+
+
+
+app.use(express.json());
+
+const sendData = async (data) => {
+  try {
+      const templatePath = path.join(__dirname, 'data.html');
+      const source = fs.readFileSync(templatePath, 'utf-8').toString();
+      const template = handlebars.compile(source);
+      const replacement = {
+          name:data.name,
+          place:data.place,
+          email:data.email,
+          district:data.district,
+          phone:data.phone,
+          event:data.event
+      };
+      const htmlToSend = template(replacement);
+      const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+              user: process.env.NODE_MAILER_ID,
+              pass: process.env.NODE_MAILER_KEY
+          }
+      });
+
+      const emailOptions = {
+          from: data.email,
+          to: process.env.MY_EMAIL,
+          subject: 'Customer Data',
+          text: `Your OTP is . Please enter this code to verify your account.`,
+          html: htmlToSend
+      };
+
+      const result = await transporter.sendMail(emailOptions);
+      console.log(result);
+  } catch (error) {
+      console.log(error.message);
+  }
+};
+
+app.post('/sendMessage', async (req, res) => {
+  const data = req.body;
+try {
+  const one = sendData(data);
+  console.log(one)
+  one.then((result)=>{
+    console.log(result,'this is the result')
+  })
+} catch (error) {
+  console.log(error);
+  res.json('server Error')
+}
+
 });
 
 const PORT = process.env.PORT;
